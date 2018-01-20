51a5293377d451974f762f99b8c82ca4e74d4904
Orkhan Farajov <orkhankhf@code.edu.az>
2018-01-21 03:37:43 +0400

    n changed again

---
+++ Mini-max-sum/Mini-max-sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mini_max_sum
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            long[] arr = Array.ConvertAll(arr_temp, Int64.Parse);
+            long minSum = arr.Sum() - arr.Max();
+            long maxSum = arr.Sum() - arr.Min();
+            Console.WriteLine("{0} {1}", minSum, maxSum);
+        }
+    }
+}
